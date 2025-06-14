0ec5a2946eb10f605a84aea0cfab7c82f6135743
Roonil03 <aryanvivek05@gmail.com>
2025-06-14 19:25:40 +0530

    bonus problem -1 edited

--- BonusProblems/ProblemMinus0001/minus1.go
+++ BonusProblems/ProblemMinus0001/minus1.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("How to solve this, since the series diverges, and doesn't converge at infinity")
+	fmt.Print("This one requires actual maths. Refer to the inifnite series = -1/12 solution. Thus the answer is:")
+	fmt.Print("\n7/12")
 }
